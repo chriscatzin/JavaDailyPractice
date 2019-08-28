eb4a68a05d9f2bdcd39b1c96fcf29e9855018bd8
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-08-27 20:07:53 -0700

    Practice

---
+++ arrayListChallenge/src/com/collabera/Main.java
@@ -0,0 +1,53 @@
+package com.collabera;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("0039 330 4404");
+
+    public static void main(String[] args) {
+
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while(!quit){
+            System.out.println("\nEnter action: (6 to show " +
+                    "available actions");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(action){
+                case 0:
+                    System.out.println("\nShutting down...");
+                    quit = true;
+                    break;
+                case 1:
+                    printContacts();
+                    break;
+            }
+
+        }
+    }
+
+    private static void printContacts(){
+
+    }
+
+    private static void startPhone(){
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions(){
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to add a new contact\n" +
+                "3 - to update existing an existing contact\n" +
+                "4 - to remove an existing contact\n" +
+                "5 - query if an existing contact exists\n" +
+                "6 - to print a list of available actions");
+        System.out.println("Choose your action: ");
+    }
+}
